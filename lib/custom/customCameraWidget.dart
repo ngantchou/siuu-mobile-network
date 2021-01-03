6da9ff2aac31a11b8084ded50d87ecd16e426e3f
joel <danil.ngantchou@gmail.com>
2021-01-03 08:05:24 +0100

    custom camera

--- lib/custom/customCameraWidget.dart
+++ lib/custom/customCameraWidget.dart
@@ -73,7 +73,6 @@ class _CameraWidgetState extends State<CameraWidget>
   @override
   void initState() {
     super.initState();
-<<<<<<< HEAD
     _iconsAnimationController = AnimationController(
       vsync: this,
       duration: Duration(milliseconds: 300),
@@ -93,218 +92,19 @@ class _CameraWidgetState extends State<CameraWidget>
         reverseCurve: Curves.elasticIn,
       ),
     );
-=======
->>>>>>> 1751480f647ac2ac67160b13751c11ed2c84e9c8
   }
 
   @override
   void dispose() {
-<<<<<<< HEAD
     _iconsAnimationController.dispose();
     _previewAnimationController.dispose();
     // previewStreamSub.cancel();
     _photoSize.dispose();
     _captureMode.dispose();
-=======
->>>>>>> 1751480f647ac2ac67160b13751c11ed2c84e9c8
     super.dispose();
   }
 
   @override
-<<<<<<< HEAD
-=======
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    // App state changed before we got the chance to initialize.
-    if (controller == null || !controller.value.isInitialized) {
-      return;
-    }
-    if (state == AppLifecycleState.inactive) {
-      controller?.dispose();
-    } else if (state == AppLifecycleState.resumed) {
-      if (controller != null) {
-        onNewCameraSelected(controller.description);
-      }
-    }
-  }
-
-  Widget cameraPreview() {
-    if (controller == null || !controller.value.isInitialized) {
-      return Align(
-        alignment: Alignment.center,
-        child: Text(
-          'Loading',
-          style: TextStyle(
-              color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
-        ),
-      );
-    }
-
-    return AspectRatio(
-      aspectRatio: controller.value.aspectRatio,
-      child: CameraPreview(controller),
-    );
-  }
-
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  @override
-  Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
-    final double width = MediaQuery.of(context).size.width;
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        body: SizedBox(
-          height: height,
-          width: width,
-          child: Stack(
-            children: [
-              Positioned.fill(child: cameraPreview()),
-              Positioned.fill(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SvgPicture.asset('assets/svg/flash.svg'),
-                          SvgPicture.asset('assets/svg/HDR.svg'),
-                          Row(
-                            children: [
-                              SvgPicture.asset('assets/svg/timer.svg'),
-                              SizedBox(width: width * 0.012),
-                              Text(
-                                "3s",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontFamily: "Segoe UI",
-                                  fontSize: 14,
-                                  color: Color(0xff4d0cbb),
-                                ),
-                              )
-                            ],
-                          ),
-                          SvgPicture.asset('assets/svg/filters.svg'),
-                        ],
-                      ),
-                    ),
-                    Spacer(),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: height * 0.024,
-                          width: width * 0.8,
-                          child: Stack(
-                            children: [
-                              Container(
-                                height: height * 0.024,
-                                width: width * 0.160,
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                      begin: Alignment.centerLeft,
-                                      colors: [Colors.black, Colors.white],
-                                      end: Alignment.centerRight),
-                                ),
-                              ),
-                              CarouselSlider(
-                                options: CarouselOptions(
-                                  viewportFraction: 0.3,
-                                  scrollDirection: Axis.horizontal,
-                                  height: height * 0.024,
-                                ),
-                                items: [
-                                  'PHOTO',
-                                  'SQUARE',
-                                  'VIDEO',
-                                  'PANO',
-                                ].map((i) {
-                                  return Builder(
-                                    builder: (BuildContext context) {
-                                      return Container(
-                                        child: buildText(
-                                          '$i',
-                                        ),
-                                      );
-                                    },
-                                  );
-                                }).toList(),
-                              ),
-                              // ListView(
-                              //   scrollDirection: Axis.horizontal,
-                              //   children: [
-                              //     buildText('PHOTO'),
-                              //     buildText('SQUARE'),
-                              //     buildText('VIDEO'),
-                              //     buildText('PANO'),
-                              //   ],
-                              // ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Image.asset('assets/images/photoCaptured.png'),
-                              Container(
-                                height: height * 0.096,
-                                width: width * 0.160,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  border: Border.all(
-                                    width: width * 0.014,
-                                    color: Color(0xffffffff),
-                                  ),
-                                ),
-                                child: Center(
-                                  child: Container(
-                                    height: height * 0.073,
-                                    width: width * 0.121,
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      color: Color(0xffffffff),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SvgPicture.asset('assets/svg/swap.svg'),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Row buildText(String name) {
-    final double width = MediaQuery.of(context).size.width;
-    return Row(
-      children: [
-        SizedBox(
-          width: width * 0.072,
-        ),
-        Text(
-          name,
-          style: TextStyle(
-            fontFamily: "Segoe UI",
-            fontSize: 15,
-            color: Color(0xffffffff),
-          ),
-        ),
-      ],
-    );
-  }
-  /* @override
->>>>>>> 1751480f647ac2ac67160b13751c11ed2c84e9c8
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
